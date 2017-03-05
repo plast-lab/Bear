e6d01db6a30af28a16bad0aeb2e67dda12395176
Laszlo Nagy <rizsotto@gmail.com>
2017-03-05 14:07:49 +1100

    draft command execution

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@
 extern crate clap;
 
 use std::env;
+use std::process;
 use clap::{App, Arg};
 
 fn main() {
@@ -45,7 +46,7 @@ fn main() {
             .takes_value(true)
             .value_name("directory")
             .help("Makes the database entries relative to this directory"))
-        .arg(Arg::with_name("command")
+        .arg(Arg::with_name("build")
             .value_name("command")
             .takes_value(true)
             .multiple(true)
@@ -53,8 +54,16 @@ fn main() {
             .help("The build command to intercept"))
         .get_matches();
 
-    let cmds: Vec<_> = matches.values_of("command").unwrap().collect();
-    for c in &cmds {
-        println!("{}", c);
+    let build: Vec<_> = matches.values_of("build").unwrap().collect();
+
+    let command = process::Command::new(build[0])
+        .args(&build[1..])
+        .spawn();
+    match command {
+        Ok(mut child) => match child.wait() {
+            Ok(status_code) => process::exit(status_code.code().unwrap_or(130)), // 128 + signal
+            Err(_) => process::exit(64), // not used yet
+        },
+        Err(_) => process::exit(127), // command not found
     }
 }
